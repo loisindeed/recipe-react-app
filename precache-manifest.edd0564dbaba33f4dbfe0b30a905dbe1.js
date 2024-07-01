69c8ad33136a043a7140796edc05f4005c502a97
lois <sloi@indeed.com>
2024-07-01 13:43:22 +0900

    Updates

---
+++ precache-manifest.edd0564dbaba33f4dbfe0b30a905dbe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d06af29d1a39c62ca3fbd533534ba05",
+    "url": "/recipe-react-app/index.html"
+  },
+  {
+    "revision": "d997ad14a902c79d64dc",
+    "url": "/recipe-react-app/static/css/main.87ecefc1.chunk.css"
+  },
+  {
+    "revision": "d5b4d0fb8be146f2f199",
+    "url": "/recipe-react-app/static/js/2.07acb89f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/recipe-react-app/static/js/2.07acb89f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d997ad14a902c79d64dc",
+    "url": "/recipe-react-app/static/js/main.cba594e7.chunk.js"
+  },
+  {
+    "revision": "b871f138e86ce3c464dc",
+    "url": "/recipe-react-app/static/js/runtime-main.afd009c6.js"
+  }
+]);
